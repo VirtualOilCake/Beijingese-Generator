8aa7c387788aeafcb33dadb545c0fc7e8f234490
manoil <34388992+manoil@users.noreply.github.com>
2019-12-31 21:34:16 +0800

    更改变量名

--- Beijingese-Generator.java
+++ Beijingese-Generator.java
@@ -2,22 +2,22 @@ import java.util.*;
 public class main {
     public static void main(String[] args){
 
-        Scanner input=new Scanner(System.in);//输入
-        String temp=input.next();
-        ArrayList output=new ArrayList();//输出
-        for(int i=0;i<temp.length();i-=-((i+1)/(i+1)))
+        Scanner input=new Scanner(System.in);
+        String inputString=input.next();//用户输入
+        ArrayList output=new ArrayList();//输出的list
+        for(int i=0;i<inputString.length();i=i-(-((i+1)/(i+1))))
         {
-            String test=temp.substring(i,i+1);
-            if(test.compareTo("你")==0)
+            String temp=inputString.substring(i,i+1);
+            if(temp.compareTo("你")==0)//判定是否为"你"
             {
                 output.add("您儿");
             }
-            else if(test.compareTo("A")>=0&&test.compareTo("z")<=0)
+            else if(temp.compareTo("A")>=0&&temp.compareTo("z")<=0)//判断是否为英文字符
             {
-                output.add(test+"r");
+                output.add(temp+"r");
             }
             else {
-                output.add(test+"儿");
+                output.add(temp+"儿");//其他字符
             }
         }
         System.out.println('\n'+"输出：");
